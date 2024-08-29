55d335e1579b9c3ed2467bc444985f32893cbca6
brahman89 <partnergroup21@gmail.com>
2024-08-30 01:25:52 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,10 @@
+print('Введите три числа ниже')
+first = input("Первое число :")
+second = input("Второе число :")
+third = input("Третье число :")
+if first==second and second==third:
+    print(3)
+elif first == second or second == third:
+    print(2)
+else:
+    print(1)
