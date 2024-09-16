9c85b6d4c514457746bc9054dc99856df6c5cf43
brahman89 <partnergroup21@gmail.com>
2024-09-16 20:40:17 +0700

    Add files via upload

--- modul_2_hard.py
+++ modul_2_hard.py
@@ -1,54 +1,9 @@
 import random
-def rock():
-    r = 20
-    R1 = list(range(3, r+1))
-    r = random.choice(R1)
-    return r
-rock()
-
-from operator import truediv
-
-n = rock()
-L1 = list(range(1, n + 1))
-L2 = []
-VS = []
-VS2 = []
-LS = []
-J = []
-D = {}
-for i in L1:
-    for j in range(1, n + 1):
-        n1 = L1[i - 1]
-        n2 = j
-        n3 = n1 + n2
-        J.append(j)
-        L2.append(n3)
-        VS.append([L1[i - 1],j])
-#print(VS)
-#print(range(len(VS)))
-
-m = 0
-while m < len(VS):
-    m += 1
-    l = 0
-    while VS[l] == VS[m - 1] or VS[l - 1] != VS[m - 1][::-1]:
-        l += 1
-        continue
-    else:
-        VS.pop(m - 1)
-        L2.pop(m - 1)
-
-from collections import defaultdict
-D = defaultdict(list)
-for k, v in zip(L2, VS):
-    D[k].append(v)
-
-#print(J)
-#print(VS)
-#print(L2)
-print(D)
-for s in D:
-
-
-
-
+random_value = random.randint(3,20)
+password = ""
+for i in range(1, random_value):
+    for j in range(i, random_value):
+        if random_value % (i + j) == 0:
+            password += f'{str(i)} + {str(j)}  '
+#print(password)
+print(f'{random_value} - {password}')
