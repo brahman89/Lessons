14fe6affd482a93adb8ab4ae0b57629842f10e02
brahman89 <partnergroup21@gmail.com>
2024-09-17 20:19:11 +0500

    Add files via upload

--- modul_2_hard.py
+++ modul_2_hard.py
@@ -2,8 +2,9 @@ import random
 random_value = random.randint(3,20)
 password = ""
 for i in range(1, random_value):
-    for j in range(i, random_value):
+    for j in range(i+1, random_value):
         if random_value % (i + j) == 0:
-            password += f'{str(i)} + {str(j)}  '
+            password += f'{i}{j}'
 #print(password)
 print(f'{random_value} - {password}')
+print('20 - 13141911923282183731746416515614713812911')#для проверки
