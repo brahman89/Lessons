cea64fce881ad922378fed2a3b582c089fda52f0
brahman89 <partnergroup21@gmail.com>
2024-11-20 21:33:23 +0500

    Add files via upload

--- module_9_7.py
+++ module_9_7.py
@@ -2,9 +2,11 @@ def is_prime(func):
     def wrapper(*args, **kwargs):
         m = []
         result = func(*args, **kwargs)
-        for i in range(2, func() + 1):
-            if n % i == 0:
+        for i in range(2, result + 1):
+            if result % i == 0:
                 m.append(1)
+        # print(result)
+        # print(len(m))
         if len(m) > 2:
             print("Составное")
         else:
