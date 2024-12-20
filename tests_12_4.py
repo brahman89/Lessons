1a9e1fad9541f5ff1a64663f6bcdbd28b2fa159d
brahman89 <partnergroup21@gmail.com>
2024-12-21 00:02:44 +0500

    Add files via upload

--- tests_12_4.py
+++ tests_12_4.py
@@ -1,5 +1,11 @@
 import logging
 import unittest
+from unittest import TestCase
+
+logging.basicConfig(level=logging.INFO, filemode='w', filename='runner_tests.log', encoding='utf-8',
+                        format='%(asctime)s | %(name)s | %(levelname)s | %(message)s')
+logger = logging.getLogger(__name__)
+
 
 class Runner:
     def __init__(self, name, speed=5):
@@ -32,75 +38,51 @@ class Runner:
             return self.name == other.name
 
 
-class Tournament:
-    def __init__(self, distance, *participants):
-        self.full_distance = distance
-        self.participants = list(participants)
+# class Tournament:
+#     def __init__(self, distance, *participants):
+#         self.full_distance = distance
+#         self.participants = list(participants)
+#
+#     def start(self):
+#         finishers = {}
+#         place = 1
+#         while self.participants:
+#             for participant in self.participants[:]:
+#                 participant.run()
+#                 if participant.distance >= self.full_distance:
+#                     finishers[place] = participant
+#                     place += 1
+#                     self.participants.remove(participant)
+#
+#         return finishers
 
-    def start(self):
-        finishers = {}
-        place = 1
-        while self.participants:
-            for participant in self.participants[:]:
-                participant.run()
-                if participant.distance >= self.full_distance:
-                    finishers[place] = participant
-                    place += 1
-                    self.participants.remove(participant)
 
-        return finishers
+class RunnerTest(TestCase):
 
-class RunnerTest(unittest.TestCase):
 
     def test_walk(self):
         try:
             first = Runner('Вося', -10)
-            logging.info('"test_walk" выполнен успешно')
+            first.walk()
+            self.assertEqual(first.distance, 10)
         except:
-            logging.warning("Неверная скорость для Runner")
+            logger.warning("Неверная скорость для Runner")
 
 
     def test_run(self):
         try:
             second = Runner(5, 5)
-            logging.info('"test_walk" выполнен успешно')
-        except:
-            logging.warning("Неверный тип данных для объекта Runner")
+            second.run()
+            self.assertEqual(second.distance, 10)
+        except :
+            logger.warning("Неверный тип данных для объекта Runner")
 
-
-# class TournamentTest(unittest.TestCase):
-#
-#     def setUp(self):
-#         self.runner_1 = Runner("Усэйн", 10)
-#         self.runner_2 = Runner("Андрей", 9)
-#         self.runner_3 = Runner("Ник", 3)
-#
-#     @classmethod
-#     def setUpClass(cls):
-#         cls.all_results = dict()
-#
-#     @classmethod
-#     def tearDownClass(cls):
-#         print(*cls.all_results.values(), sep="\n")
-#
-#     def test1_on_dist_90(self):
-#         self.tournament_1 = Tournament(90, self.runner_1, self.runner_2, self.runner_3)
-#         self.all_results[1] = (self.tournament_1.start())
-#         self.assertTrue(self.all_results[1][max(self.all_results[1])] == self.runner_3.name)
 
 
 if __name__ == "__main__":
-
-    logging.basicConfig(level=logging.INFO, filemode="w", filename="runner_tests.log", encoding="UTF-8", format="%(levelname)s | %(message)s")
-
+    unittest.main()
 
 
-    # SportST = unittest.TestSuite()
-    # SportST.addTest(unittest.TestLoader().loadTestsFromTestCase(RunnerTest.test_walk()))
-    # SportST.addTest(unittest.TestLoader().loadTestsFromTestCase(RunnerTest.test_run()))
-    #
-    # runner = unittest.TextTestRunner(verbosity=2)
-    # runner.run(SportST)
 
 # first = Runner('Вося', -10)
 # second = Runner(5, 5)
